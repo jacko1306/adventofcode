040618345a58b9ce0d9e4b909ea2d3b09ed2c929
Florian Jacke <florian.jacke@kinexon.com>
2021-12-22 16:35:48 +0100

    day 12 - middle of part 2

--- 2021/aoc12.py
+++ 2021/aoc12.py
@@ -11,7 +11,7 @@ caves = set(list(itertools.chain.from_iterable(connections_list)))
 small_caves = list(filter(lambda x: x.islower(), caves))
 big_caves = list(filter(lambda x: x.isupper(), caves))
 
-for big in big_caves:
+for big in caves:
     for a, b in connections_list1:
         if a == big:
             if b != "end":
@@ -24,8 +24,26 @@ for s,e in connections_list:
     else:
         connections[s].append(e)
 
+def check_max_one_small_cave_double(node, small_visited):
+    if node == 'start':
+        return False
+    if any(value==3 for value in small_visited.values()):
+        return False
+    number = sum(value==2 for value in small_visited.values())
+    if number <= 1:
+        return True
+    return False
 
-def find_all_paths(graph, start, end, path=[], small_caves_visited=[]):
+def reset_small_cave_counter(small_cave_counter: dict):
+    for k,v in small_cave_counter.items():
+        if v >= 100:
+            small_cave_counter[k] = 100
+        else:
+            small_cave_counter[k] = 0
+    return small_cave_counter
+
+
+def find_all_paths(graph, start, end, path=[], small_caves_visited={}):
         path = path + [start]
         if start == end:
             return [path]
@@ -35,11 +53,13 @@ def find_all_paths(graph, start, end, path=[], small_caves_visited=[]):
         for node in graph[start]:
             if node not in path:
                 if node in small_caves:
-                    small_caves_visited.append(node)
+                    small_caves_visited[node] += 1
                 newpaths = find_all_paths(graph, node, end, path, small_caves_visited)
                 for newpath in newpaths:
                     paths.append(newpath)
-            elif node not in small_caves_visited:
+            elif check_max_one_small_cave_double(node,small_caves_visited):
+                if node in small_caves:
+                    small_caves_visited[node] += 1
                 newpaths = find_all_paths(graph, node, end, path, small_caves_visited)
                 for newpath in newpaths:
                     paths.append(newpath)
@@ -47,9 +67,16 @@ def find_all_paths(graph, start, end, path=[], small_caves_visited=[]):
 
 
 paths = []
-test = find_all_paths(connections, 'start','end')
+visited = {}
+for cave in small_caves:
+    visited[cave] = 0
+visited['start'] = 100
+visited['end'] = 100
+test = find_all_paths(connections, 'start', 'end',[], visited)
 for finding in test:
     paths.append(finding)
 
 for path in paths:
     print(path)
+
+print(len(paths))
