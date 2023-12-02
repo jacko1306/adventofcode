332bbfd982309907ed7ad6dbd169dab450bbcdf6
jacko1306 <45062242+jacko1306@users.noreply.github.com>
2023-12-02 23:29:51 +0100

    Add files via upload
    aoc1

---
+++ 2023/1aoc.py
@@ -0,0 +1,65 @@
+with open("input1aoc.txt") as f:
+    puzzle = f.readlines()
+
+res = 0
+spelled_numbers = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+
+def rreplace(s, old, new, occurrence):
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+
+for line in puzzle:
+    position_found_dict_low = {}
+    for spelled in spelled_numbers:
+        lowest_spot_where_substring_found = 10000
+        if spelled in line and line.find(spelled) < lowest_spot_where_substring_found:
+            lowest_spot_where_substring_found = line.find(spelled)
+            position_found_dict_low[lowest_spot_where_substring_found] = spelled
+    position_found_dict_high = {}
+    for spelled in spelled_numbers:
+        highest_spot_where_substring_found = 0
+        if spelled in line and line.rfind(spelled) > highest_spot_where_substring_found:
+            highest_spot_where_substring_found = line.rfind(spelled)
+            position_found_dict_high[highest_spot_where_substring_found] = spelled
+    if len(position_found_dict_low) > 0:
+        first_spelled_number = position_found_dict_low[min(position_found_dict_low)]
+
+        original_line = line
+        if first_spelled_number in original_line:
+            line = line.replace(
+                first_spelled_number,
+                str(spelled_numbers.index(first_spelled_number) + 1)
+                + first_spelled_number,
+                1,
+            )
+    if len(position_found_dict_high) > 0:
+        last_spelled_number = position_found_dict_high[max(position_found_dict_high)]
+        if last_spelled_number in original_line:
+            line = rreplace(
+                line,
+                last_spelled_number,
+                last_spelled_number
+                + str(spelled_numbers.index(last_spelled_number) + 1),
+                1,
+            )
+    only_numbers = [int(i) for i in line if i.isdigit()]
+    if len(only_numbers) > 0:
+        first = int(str(only_numbers)[1])
+        last = int(str(only_numbers)[-2])
+        combined = str(first) + str(last)
+
+        res += int(combined)
+
+print(res)
