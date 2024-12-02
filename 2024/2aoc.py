e3e745b55b786703fdc23988f815f93cc371e55e
Florian Jacke <flo.jacke@gmail.com>
2024-12-02 08:43:23 +0100

    day 2

--- 2024/2aoc.py
+++ 2024/2aoc.py
@@ -6,9 +6,9 @@ def isSafe(levels):
     diff_list = []
     for x, y in zip(levels[0::], levels[1::]):
         diff_list.append(y - x)
-    if max(diff_list) < 0 and min(diff_list) >= -3:    
+    if max(diff_list) < 0 and min(diff_list) >= -3:
         return 1
-    elif (min(diff_list) > 0 and max(diff_list) <= 3):
+    elif min(diff_list) > 0 and max(diff_list) <= 3:
         return 1
     else:
         return 0
@@ -22,10 +22,11 @@ for levels in reports:
 
 print(result)
 
+
 def getSubLevel(levels):
     sublevels = []
     for x in range(len(levels)):
-        sublevels.append(levels[:x-len(levels)] + levels[x+1:])
+        sublevels.append(levels[: x - len(levels)] + levels[x + 1 :])
     return sublevels
 
 
@@ -33,7 +34,7 @@ result2 = 0
 for levels in reports:
     for sublevel in getSubLevel(list(map(int, levels.split(" ")))):
         if isSafe(sublevel) == 1:
-            result2 +=1 
+            result2 += 1
             break
 
 print(result2)
