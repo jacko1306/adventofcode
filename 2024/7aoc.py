0c9adb5bad7b428f6a5a92ade6a0e82ff0a16029
Florian Jacke <flo.jacke@gmail.com>
2024-12-13 19:08:41 +0100

    day 7 part 1

---
+++ 2024/7aoc.py
@@ -0,0 +1,52 @@
+import itertools
+from operator import mul
+from functools import reduce
+
+with open("input7.txt") as f:
+    equations = f.read().splitlines()
+
+final = 0
+
+
+def mul_sum(value_list, test_result):
+    operators = ["*", "+"]
+    for operator_product in itertools.product(operators, repeat=len(value_list) - 1):
+        test = (
+            [
+                x
+                for x in itertools.chain.from_iterable(
+                    itertools.zip_longest(value_list, operator_product)
+                )
+                if x
+            ]
+        )
+        calculated = int(test[0])
+        current_operator = ''
+        for x in test[1:]:
+            if x == '+':
+                current_operator = '+'
+                continue
+            elif x == '*':
+                current_operator = '*'
+                continue
+            else:
+                if current_operator == '*':
+                    calculated *= int(x)
+                else:
+                    calculated += int(x)
+        if calculated == test_result:
+            return test_result
+    return 0
+
+        
+
+
+for equation in equations:
+    test_result, values_all = equation.split(": ")
+    test_result = int(test_result)
+    values = [a for a in values_all.split(" ")]
+    final += mul_sum(values, test_result)
+    
+
+
+print(final)
